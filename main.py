824fa7bfe91059a8b9a7d3d95cef8d62d0ddc1f9
Sandra Trošina <pr.mish@gmail.com>
2020-01-10 12:16:47 +0200

    Pievienots health, favicon
    Pievienots health, favicon

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask('app')
 
-
 @app.route('/')
 def index_page():
-  return "Sveika, pasaule!"
+  return render_template("index.html")
 
+@app.route('/health')
+def health_check():
+  return "Everythong is oke!"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
